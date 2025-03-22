6c083a987badbbdfcff8d42c1a94c515a86bfcc3
Aravinth <aravinthmspv@gmail.com>
2025-03-22 05:35:11 -0400

    Use JPA and Hibernate to implement ORM

---
+++ src/main/java/com/example/goodreads/service/BookJpaService.java
@@ -0,0 +1,63 @@
+package com.example.goodreads.service;
+
+import com.example.goodreads.model.Book;
+import com.example.goodreads.repository.BookJpaRepository;
+import com.example.goodreads.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookJpaService implements BookRepository {
+
+    @Autowired
+    private BookJpaRepository bookJpaRepository;
+
+    @Override
+    public ArrayList<Book> getBooks() {
+        List<Book> bookList = bookJpaRepository.findAll();
+        ArrayList<Book> books = new ArrayList<>(bookList);
+        return books;
+    }
+
+    @Override
+    public Book getBookById(int bookId) {
+        try {
+            Book book = bookJpaRepository.findById(bookId).get();
+            return book;
+        }
+        catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public Book addBook(Book book) {
+        bookJpaRepository.save(book);
+        return book;
+    }
+
+    @Override
+    public Book updateBook(int bookId, Book book) {
+        Book newBook = getBookById(bookId);
+
+        if(book.getName() != null) newBook.setName(book.getName());
+
+        if(book.getImgUrl() != null) newBook.setImgUrl(book.getImgUrl());
+
+        bookJpaRepository.save(newBook);
+        return newBook;
+
+    }
+
+    @Override
+    public void deleteBook(int bookId) {
+        Book existingBook = getBookById(bookId);
+        bookJpaRepository.deleteById(existingBook.getId());
+        throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+    }
+}
